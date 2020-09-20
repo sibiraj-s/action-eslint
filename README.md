9c23aaa7e492d8b8f66514ab471e465e9fd5acca
sibiraj-s <dev.sibiraj@outlook.com>
2020-09-20 16:31:56 +0530

    Setup ESLint action

---
+++ README.md
@@ -0,0 +1,43 @@
+# Action ESLint
+
+> GitHub Action that runs ESLint on files changed in a Pull Request.
+
+![Annotation](assets/annotation.png)
+
+## Usage
+
+`.github/workflows/lint.yml`:
+
+```yml
+name: Lint
+
+on:
+  pull_request:
+  push:
+    branches:
+      - master
+
+jobs:
+  eslint:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: actions/setup-node@v1
+        with:
+          node-version: '14'
+      - run: npm ci # or yarn install
+      - uses: sibiraj-sr/action-eslint@v1
+        with:
+          eslintArgs: '--ignore-path .gitignore --quite'
+          extensions: 'js, jsx, ts, tsx'
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+## Known Issues
+
+- Yarn 2 is not supported
+
+## Debugging
+
+To enable debug logs, set secret `ACTIONS_STEP_DEBUG` to `true`. Refer docs more details https://docs.github.com/en/actions/managing-workflow-runs/enabling-debug-logging#enabling-step-debug-logging
---
+++ src/getChangedFiles.ts
@@ -0,0 +1,63 @@
+import { getInput, debug } from '@actions/core'
+import { getOctokit, context } from '@actions/github'
+import { PullsListFilesResponseData, ReposGetCommitResponseData } from '@octokit/types'
+
+type FileList = string[]
+
+type File = {
+  filename: string;
+  additions: number;
+  deletions: number;
+  changes: number;
+  status: string;
+  raw_url: string;
+  blob_url: string;
+  patch: string;
+}
+
+const getFiles = (files: File[]): FileList => files
+  .filter((file) => file.status !== 'removed')
+  .map((file) => file.filename)
+
+const getChangedFiles = async (token: string): Promise<FileList> => {
+  const octokit = getOctokit(token)
+  const pullRequest = context.payload.pull_request
+
+  let files: FileList
+  if (!pullRequest?.number) {
+    const options = octokit.repos.getCommit.endpoint.merge({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: context.sha,
+    })
+
+    const response: ReposGetCommitResponseData[] = await octokit.paginate(options)
+    const filesArr = response.map((data) => data.files)
+
+    const filesChangedInCommit = filesArr.reduce((acc, val) => acc.concat(val), [])
+    files = getFiles(filesChangedInCommit as File[])
+  } else {
+    const options = octokit.pulls.listFiles.endpoint.merge({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: pullRequest.number,
+    })
+
+    const prResponse: PullsListFilesResponseData = await octokit.paginate(options)
+    files = getFiles(prResponse as File[])
+  }
+
+  debug('Files changed...')
+  files.forEach(debug)
+
+  const supportedExtensions = getInput('extensions').split(',')
+
+  const supportedFiles = files.filter((filename) => {
+    const isSupportedFile = supportedExtensions.find((ext) => filename.endsWith(`.${ext}`))
+    return isSupportedFile
+  })
+
+  return supportedFiles
+}
+
+export default getChangedFiles
